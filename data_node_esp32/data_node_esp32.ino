6edc586c3820459739ebc53c489e3dafcc234237
Eduardas Kazakas <eduardas.kazakas.consultant@nielsen.com>
2024-07-12 18:13:13 +0300

    WIP

--- data_node_esp32/data_node_esp32.ino
+++ data_node_esp32/data_node_esp32.ino
@@ -1,80 +1,136 @@
-
-#include <ESP8266WiFi.h>
-#include <DNSServer.h>
+#include <WiFiClientSecure.h>
 #include <WiFiManager.h>
-#include <DHT.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
+#define uS_TO_S_FACTOR 1000000
 #define DN_DEBUG true
+
+/*
+mosquitto_pub \
+  --cert /home/edkazaka2001/Downloads/iot/d817ad829713b29dd3ff7e34b4c2c8c5ddc00dcef50cad4777f8f0ca882dbe2b-certificate.pem.crt \
+  --key /home/edkazaka2001/Downloads/iot/d817ad829713b29dd3ff7e34b4c2c8c5ddc00dcef50cad4777f8f0ca882dbe2b-private.pem.key \
+  --cafile /home/edkazaka2001/Downloads/iot/AmazonRootCA1.pem \
+  -h a1j9n17qa1h23g-ats.iot.eu-central-1.amazonaws.com \
+  -p 8883 \
+  -q 0 \
+  -d \
+  -V mqttv5 \
+  -t 'esp32/sensors' \
+  -i 'esp32' \
+  -m "Hello from Mosquitto"
+
+mosquitto_sub \
+  --cert /home/edkazaka2001/Downloads/iot/8724a923afddec3e06fd4eb05e02c5e47b8172b2b19c14cb4d00c88d3a273fee-certificate.pem.crt \
+  --key /home/edkazaka2001/Downloads/iot/8724a923afddec3e06fd4eb05e02c5e47b8172b2b19c14cb4d00c88d3a273fee-private.pem.key \
+  --cafile /home/edkazaka2001/Downloads/iot/AmazonRootCA1.pem \
+  -h a1j9n17qa1h23g-ats.iot.eu-central-1.amazonaws.com \
+  -p 8883 \
+  -q 0 \
+  -d \
+  -t 'esp32/readings' \
+  -i 'esp32-sub'
+*/
 
 const uint8_t tQualifier = 1, hQualifier = 2;
 const char* deviceId = "fb5c282c-2ff9-44a2-b31b-a5fc045d9f30";
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-DHT dht(D1, DHT22);
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)EOF";
+
+static const char AWS_CERT_CRT[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----
+)EOF";
+
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"EOF(
+-----BEGIN RSA PRIVATE KEY-----
+-----END RSA PRIVATE KEY-----
+)EOF";
+
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
+
 WiFiManager wifiManager;
-WiFiManagerParameter mqttDomain("domain", "mqtt domain", "test.mosquitto.org", 64);
-WiFiManagerParameter mqttPort("port", "mqtt port", "1883", 5, "pattern='\\d{1,5}'");
-WiFiManagerParameter mqttTopic("topic", "mqtt topic", "sensor/readings", 64);
+
+WiFiManagerParameter mqttDomain("domain", "mqtt domain", "a1j9n17qa1h23g-ats.iot.eu-central-1.amazonaws.com", 128);
+WiFiManagerParameter mqttPort("port", "mqtt port", "8883", 5, "pattern='\\d{1,5}'");
+WiFiManagerParameter mqttTopic("topic", "mqtt topic", "esp32/readings", 64);
 WiFiManagerParameter sleepSeconds("sleep", "sleep (seconds)", "30", 4, "pattern='\\d{1,4}'");
 
-void setup() {
-  #ifdef DN_DEBUG
-  Serial.begin(115200);
-  while (!Serial) {
-    // some boards need this because of native USB capability
-  }
-  #endif
+RTC_DATA_ATTR int bootCount = 0;
 
-  WiFi.mode(WIFI_STA);
+#ifdef DN_DEBUG
+void printWakeupReason() {
+  esp_sleep_wakeup_cause_t wakeupReason = esp_sleep_get_wakeup_cause();
 
-  wifiManager.setConfigPortalBlocking(false);
-  wifiManager.setConfigPortalTimeout(120);
-  wifiManager.setConnectTimeout(15);
-
-  wifiManager.addParameter(&mqttDomain);
-  wifiManager.addParameter(&mqttPort);
-  wifiManager.addParameter(&mqttTopic);
-  wifiManager.addParameter(&sleepSeconds);
-
-  if (!wifiManager.autoConnect("Maskatuoklis 3000")) {
-    #ifdef DN_DEBUG
-    Serial.println(F("Could not connect to previous SSID!"));
-    #endif
-  } else {
-    #ifdef DN_DEBUG
-    Serial.println(F("Connected!"));
-    Serial.print(F("Local IP: "));
-    Serial.println(WiFi.localIP()); 
-    #endif
-
-    client.setServer(mqttDomain.getValue(), atoi(mqttPort.getValue()));
-    dht.begin();
+  switch(wakeupReason)
+  {
+    case ESP_SLEEP_WAKEUP_EXT0:
+      Serial.println(F("Wakeup caused by external signal using RTC_IO"));
+      break;
+    case ESP_SLEEP_WAKEUP_EXT1:
+      Serial.println(F("Wakeup caused by external signal using RTC_CNTL"));
+      break;
+    case ESP_SLEEP_WAKEUP_TIMER:
+      Serial.println(F("Wakeup caused by timer"));
+      break;
+    case ESP_SLEEP_WAKEUP_TOUCHPAD:
+      Serial.println(F("Wakeup caused by touchpad"));
+      break;
+    case ESP_SLEEP_WAKEUP_ULP:
+      Serial.println(F("Wakeup caused by ULP program"));
+      break;
+    default:
+      Serial.print(F("Wakeup was not caused by deep sleep: "));
+      Serial.println(wakeupReason);
+      break;
   }
 }
+#endif
+
+#ifdef DN_DEBUG
+void saveParamsCallback () {
+  Serial.println(F("Get Params:"));
+  
+  Serial.print(mqttDomain.getID());
+  Serial.print(F(" : "));
+  Serial.println(mqttDomain.getValue());
+  
+  Serial.print(mqttPort.getID());
+  Serial.print(F(" : "));
+  Serial.println(mqttPort.getValue());
+
+  Serial.print(mqttTopic.getID());
+  Serial.print(F(" : "));
+  Serial.println(mqttTopic.getValue());
+
+  Serial.print(sleepSeconds.getID());
+  Serial.print(F(" : "));
+  Serial.println(sleepSeconds.getValue());
+}
+#endif
 
 void reconnect() {
   uint8_t tries = 0;
 
-  while (!client.connected() && tries < 3) {
+  while (!mqttClient.connected() && tries < 3) {
     #ifdef DN_DEBUG
-    Serial.print(F("Attempting MQTT connection..."));
+    Serial.print(F("Attempting MQTT connection... "));
     #endif
 
-    if (client.connect("ESP8266Client")) {
+    if (mqttClient.connect("esp32")) {
       #ifdef DN_DEBUG
-      Serial.println(F("connected"));
+      Serial.println(F("Connected!"));
       #endif
     } else {
       #ifdef DN_DEBUG
-      Serial.print(F("failed, rc="));
-      Serial.print(client.state());
-      Serial.println(F(" try again in 2 seconds"));
+      Serial.print(F("Failed, rc="));
+      Serial.print(mqttClient.state());
+      Serial.println(F(" trying again."));
       #endif
-
-      delay(2000);
 
       tries++;
     }
@@ -89,29 +145,74 @@ void publish(const char* payload) {
   Serial.println(payload);
   #endif
 
-  while (!client.publish(mqttTopic.getValue(), payload, true) && tries < 3) {
+  while (!mqttClient.publish(mqttTopic.getValue(), payload) && tries < 3) {
     #ifdef DN_DEBUG
-    Serial.print(F("failed, rc="));
-    Serial.print(client.state());
-    Serial.println(F(" try again in 2 seconds"));
+    Serial.print(F("Failed, rc="));
+    Serial.print(mqttClient.state());
+    Serial.println(F(" trying again."));
     #endif
-
-    delay(2000);
 
     tries++;
   }
 }
 
+void setup() {
+  #ifdef DN_DEBUG
+  Serial.begin(115200);
+  while (!Serial); // some boards need this because of native USB capability
+  #endif
+
+  ++bootCount;
+  #ifdef DN_DEBUG
+  Serial.print(F("Boot number: "));
+  Serial.println(String(bootCount));
+  printWakeupReason();
+  #endif
+
+  WiFi.mode(WIFI_STA);
+
+  wifiClient.setCACert(AWS_CERT_CA);
+  wifiClient.setCertificate(AWS_CERT_CRT);
+  wifiClient.setPrivateKey(AWS_CERT_PRIVATE);
+
+  wifiManager.addParameter(&mqttDomain);
+  wifiManager.addParameter(&mqttPort);
+  wifiManager.addParameter(&mqttTopic);
+  wifiManager.addParameter(&sleepSeconds);
+
+  wifiManager.setConfigPortalBlocking(false);
+  wifiManager.setConfigPortalTimeout(300);
+  wifiManager.setConnectTimeout(30);
+  #ifdef DN_DEBUG
+  wifiManager.setSaveParamsCallback(saveParamsCallback);
+  #endif
+
+  if (!wifiManager.autoConnect(deviceId)) {
+    #ifdef DN_DEBUG
+    Serial.println(F("Could not connect to previous SSID!"));
+    #endif
+  } else {
+    #ifdef DN_DEBUG
+    Serial.println(F("Connected!"));
+    Serial.print(F("Local IP: "));
+    Serial.println(WiFi.localIP()); 
+    #endif
+
+    mqttClient.setServer(mqttDomain.getValue(), atoi(mqttPort.getValue()));
+//    dht.begin();
+  }
+}
+
 void loop() {
   if (!wifiManager.process()) {
-    if (!client.connected()) {
+    if (!mqttClient.connected()) {
       reconnect();
     }
 
-    if (client.loop()) {
-      float h = dht.readHumidity();
-      float t = dht.readTemperature();
-      float hic = dht.computeHeatIndex(t, h, false);
+    if (mqttClient.loop()) {
+      float h = 50.0;///dht.readHumidity();
+      float t = 20.0;//dht.readTemperature();
+      float hic = 20.0;//dht.computeHeatIndex(t, h, false);
 
       #ifdef DN_DEBUG
       Serial.print(F("Humidity: "));
@@ -134,8 +235,8 @@ void loop() {
       serializeJson(doc, output);
 
       publish(output.c_str());
-    }
 
-    ESP.deepSleep(atoi(sleepSeconds.getValue()) * 1000000);
+      delay(10000);
+    }
   }
 }
